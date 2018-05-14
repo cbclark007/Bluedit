9843895404231dd35be179380a986f7bbe42b9cc
Christopher Clark <cbclark007@gmail.com>
2018-05-14 13:22:13 -0700

    login started, some server setup changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const database = process.env.MONGODB_URI ||
-                 'mongodb://localhost:27017/loginExample';
+                 'mongodb://localhost:27017/Bluedit';
 mongoose.connect(database)
   .then(() => {
     console.log(`Database up on ${database}`);
--- server/server.js
+++ server/server.js
@@ -6,32 +6,35 @@ const morgan = require('morgan');
 const flash = require('connect-flash');
 require('dotenv').config();
 
-// const usersRoute = require('./routes/users.js');
-const mongoose = require('./db/mongoose.js');
-// const User = require('./models/user.js');
+// const usersRoute = require('../routes/users.js');
+const mongoose = require('../db/mongoose.js');
+const User = require('../models/user.js');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 // View Engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.engine('hbs', exphbs({defaultLayout : 'main',
                           extname       : '.hbs'}))
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, '../public')));
 // app.use(morgan('dev'));
-app.use()
+// app.use()
+
+app.get('/', (req, res) => {
+  res.render('login');
+})
+
 
 // Mount Routes
-app.use('/users', usersRoute);
+// app.use('/users', usersRoute);
 
 app.listen(port, () => {
   console.log(`Web server up on port ${port}`);
